6f12f826fd2130e6bca1cabfd28edf45604f0010
StevenButtifint <87138797+StevenButtifint@users.noreply.github.com>
2023-03-27 23:31:38 +0100

    added video feed frame

--- app.py
+++ app.py
@@ -10,6 +10,12 @@ class GUI:
         self.window.title(TITLE)
         self.window.iconbitmap(resource_path(APP_ICON_DIR))
 
+        video_frame = tk.Frame(self.window, bg="black")
+        video_frame.place(relx=0.01, rely=0.055, relw=0.5, relh=0.5, anchor="nw")
+        self.video_label = tk.Label(master=video_frame, bg="black", fg="grey", text="Video Feed Disabled")
+        self.video_label.place(relx=0.5, rely=0.5, relw=1, relh=1, anchor='c')
+
+
 
 if __name__ == '__main__':
     root = tk.Tk()
