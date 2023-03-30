6e2f548370d1ec0cc3af515680aea43fb8b3258a
StevenButtifint <87138797+StevenButtifint@users.noreply.github.com>
2023-03-30 18:16:45 +0100

    added event frame content

--- res/event_widget.py
+++ res/event_widget.py
@@ -16,3 +16,18 @@ class EventWidget:
         self.event_frame = tk.Label(last_event_frame, text="No Preview", bg=EVENT_PREVIEW, font=('Corbel', 12))
         self.event_frame.place(relx=0.5, rely=0.5, anchor="c")
 
+        type_title = tk.Label(self.window, text="Type:", bg=TAB_BG_SELECTED, font=('Corbel', 16))
+        type_title.place(relx=0.6, rely=0.25, anchor="e")
+        self.type = tk.Label(self.window, text=EMPTY_TYPE, bg=TAB_BG_SELECTED, font=('Corbel', 16))
+        self.type.place(relx=0.6, rely=0.25, anchor="w")
+
+        time_title = tk.Label(self.window, text="Time:", bg=TAB_BG_SELECTED, font=('Corbel', 16))
+        time_title.place(relx=0.6, rely=0.35, anchor="e")
+        self.time = tk.Label(self.window, text=EMPTY_TIME, bg=TAB_BG_SELECTED, font=('Corbel', 16))
+        self.time.place(relx=0.6, rely=0.35, anchor="w")
+
+        date_title = tk.Label(self.window, text="Date:", bg=TAB_BG_SELECTED, font=('Corbel', 16))
+        date_title.place(relx=0.6, rely=0.45, anchor="e")
+        self.date = tk.Label(self.window, text=EMPTY_DATE, bg=TAB_BG_SELECTED, font=('Corbel', 16))
+        self.date.place(relx=0.6, rely=0.45, anchor="w")
+
