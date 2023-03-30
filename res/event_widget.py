0bee6983596d6a261215fbd612835a52b10d1d32
StevenButtifint <87138797+StevenButtifint@users.noreply.github.com>
2023-03-30 18:16:17 +0100

    added last event frame

--- res/event_widget.py
+++ res/event_widget.py
@@ -10,3 +10,9 @@ class EventWidget:
     def __init__(self, parent):
         self.window = parent
 
+        last_event_frame = tk.Frame(self.window, background=EVENT_PREVIEW)
+        last_event_frame.place(relx=0.25, rely=0.5, relw=0.4, relh=0.8, anchor="c")
+
+        self.event_frame = tk.Label(last_event_frame, text="No Preview", bg=EVENT_PREVIEW, font=('Corbel', 12))
+        self.event_frame.place(relx=0.5, rely=0.5, anchor="c")
+
