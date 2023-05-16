f8e253f9e6c23aeafcda9c4013f27d7a0fbd4f70
StevenButtifint <87138797+StevenButtifint@users.noreply.github.com>
2023-05-16 20:37:24 +0100

    added widget constants

--- res/constants.py
+++ res/constants.py
@@ -3,6 +3,13 @@ TITLE = "Smart Security System"
 DIMENSIONS = "1200x800"
 
 
+# WIDGETS
+VIDEO_FEED_OPTIONS = ["CAM 1", "CAM 2", "CAM 3", "MP4"]
+DETECTOR_OPTIONS = ["YOLOv5s"]
+TAB_OPTIONS = ["Last Event", "Selected Event", "Options", "About"]
+EVENT_COLUMN_NAMES = ["Date", "Time", "Class", "Note"]
+EVENT_COLUMN_WIDTHS = [2, 2, 50, 200]
+TOOLS_HEIGHT = 0.04
 
 
 # Directories
