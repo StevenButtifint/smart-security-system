ec9a7266c12ab31ba7d71edb80f573dbd44a6922
StevenButtifint <87138797+StevenButtifint@users.noreply.github.com>
2023-05-22 14:55:32 +0100

    added scan frame method

--- res/detector.py
+++ res/detector.py
@@ -23,6 +23,41 @@ class Detector:
     def update_existing_objects(self, new_dots):
         self.existing_objects = new_dots
 
+    def scan_frame(self, captured_frame, display_frame):
+        image = np.asarray(captured_frame)
+        blob, input_image = self.format_image(image)
+
+        predictions = self.get_predictions(blob)
+        class_ids, confidences, boxes, class_id = self.filter_predictions(predictions, input_image)
+
+        indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.25, 0.45)
+        result_class_ids, result_boxes = self.get_detection_results(indexes, class_ids, boxes)
+
+        new_dots = []
+        new_detections = []
+
+        for i in range(len(result_class_ids)):
+            if self.class_list[class_id] in OBJECT_TYPES:
+                box = result_boxes[i]
+
+                # if there is an existing object dot in the area then its not a new object
+                if not self.is_existing(box):
+                    new_detections.append((self.class_list[class_id], "A " + self.class_list[class_id] + " was detected."))
+                    self.save_last_detection(image, box)
+
+                self.draw_detection_box(image, box)
+                self.save_dot(box, new_dots)
+
+                if self.show_dot:
+                    self.draw_dot(image, box)
+
+                if self.show_label:
+                    self.draw_detection_label(image, box, result_class_ids[i])
+
+        self.update_existing_objects(new_dots)
+        self.update_display_image(display_frame, image)
+        return new_detections
+
 
     @staticmethod
     def get_class_list():
